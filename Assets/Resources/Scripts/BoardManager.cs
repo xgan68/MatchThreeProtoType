4f278b22659beaf91e6080bd065bed984c61729c
xgan68 <alex.x.gan@gmail.com>
2017-10-01 22:34:53 -0700

    Setup Board and Gems

---
+++ Assets/Resources/Scripts/BoardManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+	private readonly int BOARDWIDTH = 5;
+	private readonly int BOARDHEIGHT = 5;
+
+	// Use this for initialization
+	void Start () {
+		onNewGameStart ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onNewGameStart() {
+		generateGems(BOARDWIDTH, BOARDWIDTH);
+	}
+
+
+	void generateGems(int width, int height) {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				GameObject gemGameObject = Instantiate((GameObject)Resources.Load("Prefabs/Gem"), 
+														new Vector3(x - width/2 ,y , 0), 
+														Quaternion.identity)as GameObject;
+			}
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/Gem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour {
+
+	private readonly string[] COLORMAP = {"Blue", "Red", "Green", "Yellow"};
+
+	string color;
+	GameObject gemCube;
+
+	// Use this for initialization
+	void Start () {
+		gemCube = transform.Find ("Cube").gameObject;
+		generateGem ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void generateGem() {
+		color = COLORMAP [Random.Range (0, COLORMAP.Length)];
+		Material gemMaterial = (Material)Resources.Load ("Materials/" + color);
+		gemCube.GetComponent<Renderer> ().material = gemMaterial;
+	}
+}
